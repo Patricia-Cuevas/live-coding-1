4795d0d42ea5ad8c2c5450f33b6bd6648f742495
Patricia-Cuevas <cuevasdpatricia@gmial.com>
2024-05-24 14:28:26 -0600

    ejercicio de live coding

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí.
+function bigWords(str, arr) {
+    return arr.filter(word => word.length > str.length);
+}
+
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+console.log(bigWords('bocina', myArray)); 
